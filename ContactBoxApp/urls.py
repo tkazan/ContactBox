565ee618a297e2675fbe87c321e25397dd2bf34e
Tobiasz Kazanowski <tobiaszkazanowski@gmail.com>
2018-04-25 01:42:25 +0200

    urls and views design

--- ContactBoxApp/urls.py
+++ ContactBoxApp/urls.py
@@ -5,5 +5,9 @@ from .views import *
 app_name = 'contactbox'
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^new$', NewPersonView.as_view(), name='new'),
+    url(r'^show/(?P<id>(\d)+)$', ShowPersonView.as_view(), name='person'),
+    url(r'^modify/(?P<id>(\d)+)$', ModifyPersonView.as_view(), name='modify'),
+    url(r'^delete/(?P<id>(\d)+)$', DeletePersonView.as_view(), name='delete'),
 
 ]
--- ContactBoxApp/views.py
+++ ContactBoxApp/views.py
@@ -1,8 +1,40 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
 
 
 def home(request):
     return render(request, 'ContactBox/home.html')
+
+
+class NewPersonView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class ShowPersonView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class ModifyPersonView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class DeletePersonView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
